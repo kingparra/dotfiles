d133b0bb533999caad49b43c2f6c5d2e17584865
Chris King-Parra <chris@kingparra.work>
2019-11-12 18:00:00 -0500

    Correct comment about backward-bigword

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
 bind \cw backward-kill-bigword # <Control-w> kills a word back, even if it has punctuation in it
-# ...we already have <Alt-d> to kill an unpunctuated word back
+# ...we already have <Alt-Backspace> to kill an unpunctuated word back
 bind \eB backward-bigword # <Alt-Shift-b> moves back a word, even if it has punctuation in it
 # ...we already have <Alt-b> to move an unpunctuated word back
